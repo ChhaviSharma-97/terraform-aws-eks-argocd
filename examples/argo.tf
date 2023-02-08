3f306b2015a9aa7bd7a274b6837fb052a8dc5aea
Chhavi Sharma <60739490+ChhaviSharma-97@users.noreply.github.com>
2023-02-08 21:52:34 +0530

    updated example

--- examples/argo.tf
+++ examples/argo.tf
@@ -1,45 +1,33 @@
 module "argocd" {
-  source = "git::https://github.com/DNXLabs/terraform-aws-eks-argocd.git?ref=0.3.1"
+  source = "git::https://github.com/ChhaviSharma-97/terraform-aws-eks-argocd"
 
   enabled = true
-
   helm_services = [
     {
-      name          = "argo-cd"
+      name          = "argocd"
+      chart_version = "5.16.1"
       release_name  = "argo-cd"
-      chart_version = "3.3.5"
       settings = {
         "server" = {
-          "service" = {
-            "type" = "NodePort"
-          }
+          "extraArgs" = [
+              "--insecure",
+          ]
           "ingress" = {
             "enabled" = true
             "https"   = true
-            "paths"   = ["/*"]
             "annotations" = {
-              "ingress.kubernetes.io/group.name"               = "dev-apps-private"
               "kubernetes.io/ingress.class"                    = "alb"
-              "alb.ingress.kubernetes.io/certificate-arn"      = "arn:aws:acm:us-east-1:000:certificate/000"
-              "alb.ingress.kubernetes.io/scheme"               = "internal"
+              "alb.ingress.kubernetes.io/scheme"               = "internet-facing"
+              "alb.ingress.kubernetes.io/target-type"          = "ip"
+              "alb.ingress.kubernetes.io/success-codes"        = "200"
+              "alb.ingress.kubernetes.io/healthcheck-protocol" = "HTTP"
+              "alb.ingress.kubernetes.io/certificate-arn"      = "arn:aws:acm:region-code:account-id:certificate/xyz"
               "alb.ingress.kubernetes.io/listen-ports"         = "[{\"HTTP\": 80}, {\"HTTPS\":443}]"
               "alb.ingress.kubernetes.io/actions.ssl-redirect" = "{\"Type\": \"redirect\", \"RedirectConfig\": { \"Protocol\": \"HTTPS\", \"Port\": \"443\", \"StatusCode\": \"HTTP_301\"}}"
-              "alb.ingress.kubernetes.io/target-type"          = "instance"
-              "alb.ingress.kubernetes.io/success-codes"        = "200"
-              "alb.ingress.kubernetes.io/group.order"          = "51"
-              "alb.ingress.kubernetes.io/healthcheck-protocol" = "HTTPS"
-              "alb.ingress.kubernetes.io/backend-protocol"     = "HTTPS"
             }
-            "hosts" = ["argocd.dev.private.mydomain.com"]
           }
         }
       }
-    },
-    {
-      name          = "argo-rollouts"
-      release_name  = "argo-rollouts"
-      chart_version = "0.5.2"
-      settings      = {}
     }
   ]
 }
